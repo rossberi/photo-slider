bf1ac0f09475e756f934ba32a8cab6a3a2550f3a
Richard <richard@rossberg.info>
2026-03-01 16:38:17 +0100

    🚀 added: token and url test for security

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -6,13 +6,34 @@ from fastapi import Request
 import os
 import random
 
+token = os.environ.get("TOKEN")
+if not token:
+    raise Exception("TOKEN not set")
+
+hostname = os.environ.get("HOSTNAME")
+if not hostname:
+    raise Exception("HOSTNAME not set")
+
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+@app.middleware("http")
+async def restrict_access(request: Request, call_next):
+    allowed_path = f"/{token}"
 
-@app.get("/", response_class=HTMLResponse)
+    request_host = request.headers.get("host", "")
+    if request_host != hostname:
+        return Response(status_code=404)
+
+    path = request.url.path
+    if path != allowed_path and not path.startswith("/static/"):
+        return Response(status_code=404)
+
+    return await call_next(request)
+
+@app.get(f"/{token}", response_class=HTMLResponse)
 async def home(request: Request):
     image_folder = "static/images"
     images = [
